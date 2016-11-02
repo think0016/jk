9f771f153272595686e2ea86f02b9aa2607e54e8
liuquan <darkmm09@163.com>
2016-11-02 23:07:47 +0800

    110223修改

---
+++ Application/Jk/Controller/TaskController.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Jk\Controller;
+use Think\Controller;
+class TaskController extends BaseController {
+    public function index(){
+    	
+    	//检查登录情况
+    	$this->is_login(1);
+    	
+    	$this->assign("sitetitle",C('sitetitle'));
+        $this->display();
+    }
+    
+    public function create() {
+    	$this->is_login(1);
+    	 
+    	$this->assign("sitetitle",C('sitetitle'));
+    	$type = I("get.type");
+    	if($type=="http"){
+    		$this->display('httpadd');
+    	}
+    	
+    }
+}
