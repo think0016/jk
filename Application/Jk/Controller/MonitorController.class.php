65a9308b0f425627ffd2cf1855adec9a87065294
liuquan <darkmm09@163.com>
2017-01-14 16:35:55 +0800

    fix

--- Application/Jk/Controller/MonitorController.class.php
+++ Application/Jk/Controller/MonitorController.class.php
@@ -183,6 +183,7 @@ class MonitorController extends BaseController {
 		// $c="/usr/bin/rrdtool fetch ".$filename." AVERAGE -r 300 -s ".$stime." -e ".$etime;
 		$c = "sh /var/www/ce/cmd/getdata.sh \"" . $fn . "\" " . $stime . " " . $etime . " " . $step;
 		exec ( $c, $output );
+		wlog("<CTCTCTCTC>".$c);
 		return $output;
 	}
 	public function getrrdfilename($tid, $uid, $mid, $sid, $ssid, $itemid, $type = 0) {
